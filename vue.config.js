5dd770e70b8b100543c5a7b25e6a48daf616d993
mattia-wsl <mat.ronzoni@gmail.com>
2021-10-21 23:38:06 +0200

    set public path

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,5 @@ module.exports = {
     }
   },
   outputDir: "./docs/",
-  publicPath: process.env.NODE_ENV === "production" ? "/docs/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/repo_index/" : "/"
 };
